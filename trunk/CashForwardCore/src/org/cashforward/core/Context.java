5a9219d342b042158442e29f6fa7b5d5f63b0d9f
wsnyder6 <wsnyder6@d41c43fc-bb4c-0410-9c24-b16605d81f76>
2008-05-29 01:36:12 +0000

    Adding 3rd party libraries

---
+++ trunk/CashForwardCore/src/org/cashforward/core/Context.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cashforward.core;
+
+import org.openide.util.Lookup.Result;
+import org.openide.util.lookup.AbstractLookup;
+import org.openide.util.lookup.InstanceContent;
+/**
+ * 
+ * @author Bill 
+ */
+public class Context extends AbstractLookup {
+
+    private InstanceContent content = null;
+    private static Context ctx = new Context();
+
+    public Context() {
+        this(new InstanceContent());
+    }
+
+    public Context(InstanceContent content) {
+        super(content);
+        this.content = content;
+    }
+
+    public void add(Object instance) {
+        content.add(instance);
+    }
+
+    public void remove(Object instance) {
+        content.remove(instance);
+    }
+
+    public static Context getDefault() {
+        return ctx;
+    }
+
+}
