2c01b26bc8c166f2f973738b2f3e150d5b122b32
wsnyder6 <wsnyder6@d41c43fc-bb4c-0410-9c24-b16605d81f76>
2008-05-29 02:13:12 +0000

    Cleaning up svn

---
+++ trunk/CashForwardModel/src/org/cashforward/model/Label.java
@@ -0,0 +1,79 @@
+package org.cashforward.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ * Labels are tags that are applied to <code>Payment</code>s. 
+ * This provides a simplistic way to categorize each Payment.
+ * 
+ * @author Bill
+ */
+@Entity
+@Table(name = "LABEL")
+@NamedQueries({@NamedQuery(name = "Label.findById", query = "SELECT l FROM Label l WHERE l.id = :id"), @NamedQuery(name = "Label.findByName", query = "SELECT l FROM Label l WHERE l.name = :name")})
+public class Label implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private long id;
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    public Label() {
+    }
+
+    public Label(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        //hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Label)) {
+            return false;
+        }
+        Label other = (Label) object;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "cashfxpersistence.Label[id=" + id + "]";
+    }
+
+}
---
+++ trunk/CashForwardModel/src/org/cashforward/service/PaymentSearchCriteria.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.cashforward.service;
+
+/**
+ *
+ * @author Bill 
+ */
+public class PaymentSearchCriteria {
+    
+}
