7fd8db52d99a858c3c4b860893c92a8a90aebdc0
wsnyder6 <wsnyder6@d41c43fc-bb4c-0410-9c24-b16605d81f76>
2008-05-29 02:10:39 +0000

    Cleaning up svn

---
+++ trunk/CashForwardUI/test/unit/src/org/cashforward/ui/adapter/PaymentServiceAdapterTest.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.cashforward.ui.adapter;
+
+import java.util.Date;
+import java.util.List;
+import org.cashforward.model.Payee;
+import org.cashforward.model.Payment;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Bill
+ */
+public class PaymentServiceAdapterTest {
+
+    public PaymentServiceAdapterTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addOrUpdatePayment method, of class PaymentServiceAdapter.
+     */
+    @Test
+    public void testAddOrUpdatePayment() {
+        System.out.println("addOrUpdatePayment");
+        PaymentServiceAdapter instance = new PaymentServiceAdapter();
+        Payment payment
+            = new Payment(4.01f,new Payee("Starbucks"),new Date());
+        payment.setOccurence("daily");
+        boolean ok = instance.addOrUpdatePayment(payment);
+        assertTrue("Payment added/updated", ok);
+        
+    }
+
+    /**
+     * Test of getAllPayments method, of class PaymentServiceAdapter.
+     */
+    @Test
+    public void testGetAllPayments() {
+        System.out.println("getAllPayments");
+        PaymentServiceAdapter instance = new PaymentServiceAdapter();
+        Payment payment
+            = new Payment(4.01f,new Payee("Starbucks"),new Date());
+        payment.setOccurence("daily");
+        boolean ok = instance.addOrUpdatePayment(payment);
+        assertTrue("Payment added/updated", ok);
+        List<Payment> result = instance.getAllPayments();
+        assertTrue("Got all payments.",result.size() > 1);
+    }
+
+}
