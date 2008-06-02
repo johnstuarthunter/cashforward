d1d2a0644b4fe3f125a59ed186639a49f394fd3d
wsnyder6 <wsnyder6@d41c43fc-bb4c-0410-9c24-b16605d81f76>
2008-06-02 02:55:14 +0000

    More calculations...

--- trunk/CashForwardModel/src/org/cashforward/service/internal/PaymentCalculator.java
+++ trunk/CashForwardModel/src/org/cashforward/service/internal/PaymentCalculator.java
@@ -22,29 +22,52 @@ public class PaymentCalculator {
         Date paymentStart = payment.getStartDate();
         Date paymentEnd = payment.getEndDate();
         Date nextPaymentDate;
-        
+
         //this needs to linked to an enum
         String occurence = payment.getOccurence();
-        
+
         //pseudocode...
         //sample
-        if ("weekly".equals(occurence)){//enum - every 7 days
+        if ("weekly".equals(occurence)) {//enum - every 7 days
             //when the range start is after the payment start
             //figure out what the earliest payment date is in the range
-            
+
             //get day difference between range start and payment start
-            //if positive, range start is after payment
-            //so, nextPaymentDate = start + (diff % 7 days)
+            int offsetStart = DateUtilities.daysBetween(start,paymentStart);
+            System.out.println("offsetStart:"+offsetStart);
+            //if positive, range start is before payment
+            //so, nextPaymentDate = start + (offset / 7 days)
+            if (offsetStart > -1)
+                nextPaymentDate = paymentStart;
+            else
+                nextPaymentDate = 
+                        DateUtilities.getDateAfterDays(start, offsetStart % 7);
+
+            //System.out.println("nextPaymentDate:"+nextPaymentDate);
             
             //after we have the nextPaymentDate, 
-            //keep adding 7 days while we are before the range end
-            
+            //keep adding 7 days while we are before the range and payment end
+            while (nextPaymentDate.before(end) && 
+                    nextPaymentDate.before(paymentEnd)) {
+                payments.add(createPayment(payment, nextPaymentDate));
+                nextPaymentDate = 
+                        DateUtilities.getDateAfterDays(nextPaymentDate, 7);
+                //System.out.println("nextPaymentDate:"+nextPaymentDate);
+                
+            }
+
         }
-        
-        
 
         return payments;
 
     }
+
+    private Payment createPayment(Payment base, Date nextDate) {
+        Payment newPayment = null;
+        newPayment = new Payment(base.getAmount(), base.getPayee(), nextDate);
+        return newPayment;
+
+    }
+
 }
 
